e95d408bda0ff8415bdc660ab7036e874b6401fa
weicong790 <weicong790@bupt.edu.cn>
2020-06-22 10:52:13 +0800

    test2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
+
 //测试github功能
 @SpringBootApplication
 public class DemoApplication {
-
+GsonBuilderUtils
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
