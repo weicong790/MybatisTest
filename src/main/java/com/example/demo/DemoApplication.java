72d8c2c7adb1adda586e456a806425c650c6c458
weicong790 <weicong790@bupt.edu.cn>
2020-06-22 10:50:45 +0800

    test2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//测试github功能
 @SpringBootApplication
 public class DemoApplication {
 
