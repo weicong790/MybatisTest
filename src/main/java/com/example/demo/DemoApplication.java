0372017b3dce52aec25c876de5b01d9e754a5225
weicong790 <weicong790@bupt.edu.cn>
2020-06-22 10:54:43 +0800

    第二个注释

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.http.converter.json.GsonBuilderUtils;
 //测试github功能
 @SpringBootApplication
 public class DemoApplication {
-    //第二个注释添加
+    //第二个注释添加a
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
