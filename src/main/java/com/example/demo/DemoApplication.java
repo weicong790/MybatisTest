fe2b6368e82c967649271380362eca305d198d53
weicong790 <weicong790@bupt.edu.cn>
2020-06-22 11:06:29 +0800

    Revert "test1"
    This reverts commit 88541edea3f86864f25e5715a3f9b653a2fca98e.
    
    # Conflicts:
    #	src/main/java/com/example/demo/DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-///test
-///test2
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
